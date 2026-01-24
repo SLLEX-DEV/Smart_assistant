ccd0596accf2ec6c7d118157dc509ed01ed25654
SLLEX <slarbidev@gmaim.com>
2026-01-24 17:25:48 +0600

    scripts update(cv2 to ai)

---
+++ core/scripts.py
@@ -0,0 +1,7 @@
+from PIL import Image
+import cv2
+import numpy as np
+
+def cv2_to_ai(camera_frame):
+    camera_frame = cv2.cvtColor(camera_frame, cv2.COLOR_BGR2RGB)
+    return Image.fromarray(camera_frame)
