d4ac0b4d8a91c5417cb79cf4c94ede14308480b6
SLLEX <slarbidev@gmaim.com>
2026-01-30 00:22:27 +0600

    func:rapid fuzz:char_compare

--- core/scripts.py
+++ core/scripts.py
@@ -2,9 +2,20 @@ from PIL import Image
 import cv2
 import numpy as np
 import json
+from rapidfuzz import process,fuzz,utils
 
 def cv2_to_ai(camera_frame):
     camera_frame = cv2.cvtColor(camera_frame, cv2.COLOR_BGR2RGB)
     return Image.fromarray(camera_frame)
 def jsonTOdict(json):
     return json.loads(json)
+def char_compare(user_input,commands):
+    result = process.extractOne(
+        user_input,
+        commands,
+        scorer=fuzz.WRatio,
+        processor=utils.default_process,
+        score_cutoff=80)
+    if result:
+        return result[0]
+    return user_input
