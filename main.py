2bfdd7f9ee350c1734acb0c5e1dea759457e22ce
SLLEX <slarbidev@gmaim.com>
2026-02-03 22:47:27 +0600

    first ai integration

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from core.scripts1 import cv2_to_ai,char_compare
 from core.vision import CameraController
 from core.voice import Voice_listener
 from core.voice_generate import tts
+from core.ai_engine import AiEngine
 import threading
 import  time
+import multiprocessing as mp
 
 #инит
 WW = wakeWord()
@@ -13,6 +15,7 @@ AM = AudioManager(devise_number=1)
 camera = CameraController()
 Vl  = Voice_listener()
 tts = tts()
+AI =AiEngine()
 
 # потоки
 thread1 = threading.Thread(target=AM.start,daemon=True)
@@ -23,14 +26,22 @@ while True:
     AM.frame_queue.task_done()
     match part:
         case 'waiting':
-                if WW.frazeDetect(audio) == 0:
+            #start_time = time.perf_counter()
+            detection = WW.frazeDetect(audio)
+            #end_time = time.perf_counter()
+            #print(f"Porcupine process time: {(end_time - start_time)*1000:.2f} ms")
+            if detection == 0:
                     print('hear')
                     part = 'listening'
         case 'listening':
                 text = Vl.Getfraze(audio.tobytes())
                 if text:
-                    print(text)
-                    part = ''
+                    part = 'generation'
+        case 'generation':
+            answer = AI.info(text)
+            if answer:
+                print(answer)
+                part = 'waiting'
 
 
 
